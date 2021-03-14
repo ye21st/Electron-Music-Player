695b3450a10baadd732797391b4f0c19765f3d54
notelist <ye21st@gmail.com>
2021-03-14 23:25:08 +0800

    提交代码

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
@@ -14,7 +14,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('renderer/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
